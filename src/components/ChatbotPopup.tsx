d8680c845aabc8bffc91e6632b2f2a2e42e8252d
Niklavs Visockis <158585129+NikVis01@users.noreply.github.com>
2025-06-03 12:33:57 +0200

    Update ChatbotPopup.tsx

--- src/components/ChatbotPopup.tsx
+++ src/components/ChatbotPopup.tsx
@@ -63,7 +63,7 @@ const ChatbotPopup = ({ initialPrompt }: ChatbotPopupProps) => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://34.68.156.74:8000/chat', {
+      const response = await fetch('https://34.68.156.74:8000/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
